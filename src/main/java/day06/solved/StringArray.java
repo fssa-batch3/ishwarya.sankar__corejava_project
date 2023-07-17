6f0cfdd3b3b2d4a1fcef055848e73a54a8a9d875
ishwaryasankar <isankar@fssa.freshworks.com>
2023-07-17 10:52:59 +0530

    commit changes

---
+++ src/main/java/day06/solved/StringArray.java
@@ -0,0 +1,27 @@
+package day06.solved;
+
+public class StringArray {
+	public static void main(String[] args) {
+
+	String[] cityArr = new String[4];
+	cityArr[0] = "Chennai";
+	cityArr[1] = "Bangalore";
+	cityArr[2] = "Mumbai";
+
+	
+	cityArr[0] = "Chennai";
+	cityArr[1] = "Bangalore";
+	cityArr[2] = "Mumbai";
+	cityArr[3]= "Delhi";
+
+
+	// Display the city names
+	for (String cityName : cityArr) {
+		System.out.println(cityName);
+	}
+	
+	
+	
+	
+}
+}
