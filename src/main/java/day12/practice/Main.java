8394f3fc0e31e7882e0bd48ee33c6509c4e928ae
ishwaryasankar <isankar@fssa.freshworks.com>
2023-07-25 19:16:35 +0530

    commit

--- src/main/java/day12/practice/Main.java
+++ src/main/java/day12/practice/Main.java
@@ -4,14 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Task task1 = new Task(14,"CompletedTask","Completed");
+		Task task1 = new Task(18,"CompletedTask","Completed");
+		Task task2 = new Task(12,"Reading Books","Pending");
+		Task task3 = new Task(10,"Dance","Completed");
 		
-		TaskDAO tsk = new TaskDAO();
+		TaskDAO tsk1 = new TaskDAO();
+		TaskDAO tsk2 = new TaskDAO();
+		TaskDAO tsk3 = new TaskDAO();
 		try {
 			System.out.println("Check");
-//		tsk.createTask(task1);
-//			tsk.updateTask(task1);
-			tsk.deleteTask(14);
+	      tsk1.createTask(task1);
+		tsk2.updateTask(task1);
+			tsk3.deleteTask(10);
 			
 		} catch (DAOException e) {
 			e.printStackTrace();
